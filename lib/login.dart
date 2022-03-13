1a9ef08ff2c7af02487d9f6bba1c3e555a4f897a
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-03-13 18:26:18 -0500

    login page is new homepage.

---
+++ lib/login.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Login")), body: Body());
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String? name;
+  TextEditingController controller = TextEditingController();
+
+  void click() {
+    name = controller.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.center,
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: "Type Your Username:",
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 5, color: Colors.black)),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.done),
+                  splashColor: Colors.orange,
+                  tooltip: "Submit",
+                  onPressed: click,
+                ),
+              ),
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
 import 'myHomePage.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(),
+      home: LoginPage(),
     );
   }
 }
