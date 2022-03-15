4f607f1d7c4391ba0841909a36d6e66ff0e43c5e
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-03-14 22:23:00 -0500

    bugfix. initializeApp() firebase method left out.

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_testing1/myHomePage.dart';
 import 'auth.dart';
@@ -21,6 +22,7 @@ class _BodyState extends State<Body> {
   late User user;
 
   void click() {
+    Firebase.initializeApp();
     signInWithGoogle().then((user) => {
           this.user = user!,
           Navigator.push(
