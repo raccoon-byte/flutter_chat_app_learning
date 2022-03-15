1359f7a34f2adccb7123f46ceca5c9b4c406a8d3
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-03-14 21:58:31 -0500

    null safety en masse to fit new Firebase API

--- lib/auth.dart
+++ lib/auth.dart
@@ -4,23 +4,24 @@ import 'package:google_sign_in/google_sign_in.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
-Future<FirebaseUser> signInWithGoogle() async {
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+Future<User?> signInWithGoogle() async {
+  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication? googleSignInAuthentication =
+      await googleSignInAccount?.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-      idToken: googleSignInAuthentication.idToken,
-      accessToken: googleSignInAuthentication.accessToken);
+  final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuthentication?.idToken,
+      accessToken: googleSignInAuthentication?.accessToken);
 
-  final AuthResult authResult = await _auth.signInWithCredential(credential);
-  final FirebaseUser user = authResult.user;
+  final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+  final User? user = authResult.user;
 
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
+  assert(!user!.isAnonymous);
+  assert(await user?.getIdToken() != null);
 
-  final FirebaseUser currentUser = await _auth.currentUser();
-  assert(currentUser.uid == user.uid);
+  final User currentUser = await _auth.currentUser!;
+  assert(currentUser.uid == user?.uid);
 
   return user;
 }
